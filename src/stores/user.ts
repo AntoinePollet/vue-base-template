6f082dc424d8093ed45f76fc1ac05e82b633141e
Antoine Pollet <antoine.pollet@idcapture.fr>
2023-08-28 13:45:40 +0200

    Init

---
+++ src/stores/user.ts
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia";
+
+const userStore = defineStore('user', {
+  
+})
+
+export default userStore;
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-amber-500 text-xl">Home</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
